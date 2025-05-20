e0adb2405ca379fa003cced073bdc03becfbff30
Güneş <gunesbalci66@hotmail.com>
2025-05-20 16:53:56 +0300

    grafik gösterimi eklendi

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("processed.cleveland.csv", na_values=['?'])
 
@@ -15,13 +16,12 @@ clean_data["thalach"] = -clean_data["thalach"]
 
 
 correlation = clean_data.corr(numeric_only=True)['num_binary'].sort_values(ascending=False)
-#print(correlation)
 strong_corr = correlation[correlation > 0.3].drop(labels=['num', 'num_binary'], errors='ignore')
-#print(strong_corr_2)
 
 
 X = clean_data[strong_corr.index]
 Y = (clean_data['num_binary'])
+mean_accuracies = []
 
 for x in range(1, 10):
     tesT_size = x/10
@@ -41,7 +41,21 @@ for x in range(1, 10):
         #conf_matrix = confusion_matrix(Y_test, Y_pred)
         #class_report = classification_report(Y_test, Y_pred)
 
-    #print(f'{x/10} Accuracy: {accuracy}')
+    mean_accuracy = np.mean(accuracies)
+    mean_accuracies.append(mean_accuracy)
+
     print(f"{tesT_size} Accuracy: {np.mean(accuracies)}")
+
+
+test_sizes = [1-(x/10) for x in range(1, 10)]
+plt.figure(figsize=(8, 5))
+plt.plot(test_sizes, mean_accuracies, marker='o', linestyle='-', color='b')
+plt.title('Test Seti Oranı vs Ortalama Doğruluk')
+plt.xlabel('Eğitim Seti Oranı')
+plt.ylabel('Ortalama Doğruluk')
+plt.grid(True)
+plt.xticks(test_sizes)
+plt.ylim(0.8, 0.88)
+plt.show()
 
 
